0d2ed3b2b285875c0b9252510fd2e4d751563cc1
Moinul Hossain <moinulhossainmahim@gmail.com>
2021-02-10 17:13:09 +0600

    Added Book and UI class in JavaScript file

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Book Class:Represents a book
+class Book {
+	constructor(title, author, isbn) {
+		this.title  = title
+		this.author = author
+		this.isbn   = isbn 
+	}
+}
+
+//UI class:Handle UI tasks
+class UI {
+	static diplayBooks() {
+		const StoredBooks = [
+			{
+				title: 'Book One',
+				author: 'John Doe',
+				isbn: 34343434
+			},
+			{
+				title: 'Book Two',
+				author: 'Jane Doe',
+				isbn: 32564562
+			}
+		]
+
+		const books = StoredBooks
+	}
+}
