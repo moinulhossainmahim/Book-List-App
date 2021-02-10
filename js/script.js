3872a1f25e4cf6a4c5fa7b761fdc326791713026
Moinul Hossain <moinulhossainmahim@gmail.com>
2021-02-10 19:28:21 +0600

    Updated UI class and added an Event

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ class Book {
 
 //UI class:Handle UI tasks
 class UI {
-	static diplayBooks() {
+	static displayBooks() {
 		const StoredBooks = [
 			{
 				title: 'Book One',
@@ -24,5 +24,42 @@ class UI {
 		]
 
 		const books = StoredBooks
+		books.forEach((book) => UI.addBookToList(book))
+	}
+
+	static addBookToList(book) {
+		const list = document.querySelector('#book-list')
+		const row  = document.createElement('tr')
+		row.innerHTMl = `
+			<td>${book.title}</td>	
+			<td>${book.author}</td>	
+			<td>${book.isbn}</td>	
+			<td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`
+		list.appendChild(row)	
 	}
 }
+
+//Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
